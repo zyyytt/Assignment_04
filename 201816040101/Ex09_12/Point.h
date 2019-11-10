0865e247a52ecd6c4c0c0841a39df36bdd562f6c
yhk-yhk <56509442+yhk-yhk@users.noreply.github.com>
2019-11-10 18:52:34 +0800

    Create Point.h

---
+++ 201816040101/Ex09_12/Point.h
@@ -0,0 +1,17 @@
+
+using namespace std;
+class Point
+{
+public:
+    Point(double,double,double,double,  double,double,double,double );
+    void setPoint(double,double,double,double,  double,double,double,double );
+    Point getPoint();
+    double Length(double ,double);
+    double Width(double ,double );
+    void square();
+private:
+    double x1,y1;
+    double x2,y2;
+    double x3,y3;
+    double x4,y4;
+};
