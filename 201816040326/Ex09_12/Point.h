bd3143143d2267101005f2c2de0c1fc0f3f3eca7
zyyytt <54845294+zyyytt@users.noreply.github.com>
2019-11-17 17:58:17 +0800

    Create Point.h

---
+++ 201816040326/Ex09_12/Point.h
@@ -0,0 +1,12 @@
+class Point
+{
+public:
+    Point(double,double);
+    void setx(double);
+    void sety(double);
+    double getx();
+    double gety();
+private:
+    double x;
+    double y;
+};
